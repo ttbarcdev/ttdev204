5ee1866643614292bf19da12d3b95890584eb044
ttdev2014 <ttdev2014@gmail.com>
2014-10-10 13:56:10 +0000

    filter for title

--- ttadminextend/ChangesReport/reportLoadProcessing.js
+++ ttadminextend/ChangesReport/reportLoadProcessing.js
@@ -28,5 +28,31 @@ $(function() {
 		$(this).html('<a href="https://admin7.testandtarget.omniture.com/admin/campaign/campaign_edit_forward.jsp?campaignId='+$(this).text()+'" target="_blank">'+$(this).text()+'</a>')
 	});
 
+	//1.8.1 fix for contains to be case insensitive
+	$.expr[":"].contains = $.expr.createPseudo(function(arg) {
+		return function( elem ) {
+			return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+		};
+	});
+
+	//Add filter logic
+	$('<div id="filterWrap">' +
+		'<label for="inpCampaignIDNameFilter" class="filterLabel">Filter by Campaign ID/Name:</label><input type="text" id="inpCampaignIDNameFilter" name="inpCampaignIDNameFilter" placeholder="Type in part of a campaign name or id, filter would apply on every keystroke. Clear to see all." />\r\n ' +
+		'</div>').insertBefore('h1.campaignNameHead:eq(0)');
+
+	//Attach event
+	$( "#inpCampaignIDNameFilter" ).keyup(function(e) {
+		var inpVal = $(this).val(),
+			selectHide="h1.campaignNamehead:not(:contains('"+inpVal+"'))", selectShow="h1.campaignNamehead:contains('"+inpVal+"')";
+		if (inpVal!=''){
+			$(selectHide).hide();
+			$(selectHide).next('table').hide();
+			$(selectShow).show();
+			$(selectShow).next('table').show();
+		}else{
+			$('h1.campaignNamehead').show().next('table').show();
+		}
+	});
+
 
 });
