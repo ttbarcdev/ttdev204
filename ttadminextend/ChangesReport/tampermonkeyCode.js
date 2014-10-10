e2c977063f1ea06e12038aab3e3d39ffcdd40a6e
ttdev2014 <ttdev2014@gmail.com>
2014-10-10 14:09:07 +0000

    update

--- ttadminextend/ChangesReport/tampermonkeyCode.js
+++ ttadminextend/ChangesReport/tampermonkeyCode.js
@@ -41,7 +41,7 @@ $(function() {
 		$('<div id="filterWrap">' +
 			'<label for="inpCampaignIDNameFilter" class="filterLabel">Filter by Campaign ID/Name:</label><input type="text" id="inpCampaignIDNameFilter" name="inpCampaignIDNameFilter" placeholder="Type in part of a campaign name or id, filter would apply on every keystroke. Clear to see all." />\r\n ' +
 			'<div></div>' +
-			'<label for="inpCampaignLogFilter" class="filterLabel">Filter by Change log keyword:</label><input type="text" id="inpCampaignLogFilter" name="inpCampaignLogFilter" placeholder="Type in part of a change log (this could be name, offer, etc), filter would apply on every keystroke. Clear to see all." />\r\n ' +
+			'<label for="inpCampaignLogFilter" class="filterLabel">Filter by Change Log keyword:</label><input type="text" id="inpCampaignLogFilter" name="inpCampaignLogFilter" placeholder="Type in part of a change log (this could be name, offer, etc), filter would apply on every keystroke. Clear to see all." />\r\n ' +
 			'</div>').insertBefore('h1.campaignNameHead:eq(0)');
 
 		//Attach event
