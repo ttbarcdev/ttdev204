425d738938af4a6a21154e6e32353de337ee5d3e
ttdev2014@gmail.com <ttdev2014@gmail.com@38c073cd-b1e0-c5ea-46b6-a719e32a6aee>
2014-10-10 13:16:02 +0000

    campaign edit links

--- ttadminextend/ChangesReport/reportLoadProcessing.js
+++ ttadminextend/ChangesReport/reportLoadProcessing.js
@@ -16,5 +16,17 @@ $(function() {
 		return false;
 	});
 
-	
+	//Check whether Tables have more than 10 rows in the tbody, remove the show/hide links for those
+	$('table tbody').each(function(){
+		if ($(this).find('tr').length <=10){
+			$(this).closest('table').find('div.showAllWrap').remove();
+		}
+	});
+
+	//Change all Campaign IDs  / titles to clickable links
+	$('h1 .campaignID').each(function(){
+		$(this).html('<a href="https://admin7.testandtarget.omniture.com/admin/campaign/campaign_edit_forward.jsp?campaignId='+$(this).text()+'" target="_blank">'+$(this).text()+'</a>')
+	});
+
+
 });
