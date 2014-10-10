0f2426a9524f81eecc1aa4c2b2120245a63cd6fe
ttdev2014@gmail.com <ttdev2014@gmail.com@38c073cd-b1e0-c5ea-46b6-a719e32a6aee>
2014-10-10 11:10:44 +0000

    move code to separate JS

--- ttadminextend/ChangesReport/reportLoadProcessing.js
+++ ttadminextend/ChangesReport/reportLoadProcessing.js
@@ -1,4 +1,20 @@
 $(function() {
 
 
+	//Show/hide links
+
+	$("a.showAllCTA:contains('show')").live("click",function(e){
+		e.preventDefault();
+		$(this).closest("table").find("tbody tr").show();
+		$(this).html("here to hide 10+ rows");
+		return false;
+	});
+	$("a.showAllCTA:contains('hide')").live("click",function(e){
+		e.preventDefault();
+		$(this).closest("table").find("tbody tr:nth-child(n+10)").hide();
+		$(this).html("here to show all rows");
+		return false;
+	});
+
+	
 });
