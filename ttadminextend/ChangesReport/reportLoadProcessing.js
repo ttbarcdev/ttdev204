a67afba7ecd40afec272ec8d2d222c70da563fb8
ttdev2014 <ttdev2014@gmail.com>
2014-10-20 15:51:27 +0000

    datepicker

--- ttadminextend/ChangesReport/reportLoadProcessing.js
+++ ttadminextend/ChangesReport/reportLoadProcessing.js
@@ -68,8 +68,11 @@ $(function() {
 			'<div></div>' +
 			'<label for="inpCampaignLogFilter" class="filterLabel">Filter by Change Log keyword:</label><input type="text" id="inpCampaignLogFilter" name="inpCampaignLogFilter" placeholder="Type in part of a change log (this could be name, offer, etc), filter would apply on every keystroke. Clear to see all." /> \r\n ' +
 			'<div></div>' +
-			'<label for="inpCampaignLogDateFilter" class="filterLabel">Filter by Change Log Date:</label><input type="text" id="inpCampaignLogDateFilter" name="inpCampaignLogDateFilter" placeholder="Date filter" /> \r\n ' +
+			'<label for="inpCampaignLogDateFilter" class="filterLabel">Filter by Change Log Date:</label><input type="text" id="inpCampaignLogDateFilter" name="inpCampaignLogDateFilter" placeholder="Date filter. Clear to see all." /> \r\n ' +
 			'</div>').insertBefore('h1.campaignNameHead:eq(0)');
+
+		//Init datepicker
+		$( "#inpCampaignLogDateFilter" ).datepicker({ dateFormat: 'DD, MM d, yy' });
 
 		//Attach events
 		$("#inpCampaignIDNameFilter").live("keyup", function(e) {
